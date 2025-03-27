0c00681839007bd6aa5a9b662ca7d72d273dfe4c
zoey-t <101599447+zt-9@users.noreply.github.com>
2025-03-27 13:54:05 -0700

    ci: tsconfig only include src

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,34 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	{ ignores: ['lib', 'node_modules', 'pnpm-lock.yaml'] },
+	{
+		ignores: [
+			'**/node_modules/**',
+			'**/dist/**',
+			'**/lib/**',
+			'**/contracts/**',
+			'**/pnpm-lock.yaml',
+		],
+	},
 	{ linterOptions: { reportUnusedDisableDirectives: 'error' } },
 	eslint.configs.recommended,
 	{
 		extends: [tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked],
-		files: ['**/*.js', '**/*.ts'],
+		files: ['src/**/*.{js,ts}'],
 		languageOptions: {
 			parserOptions: {
 				projectService: { allowDefaultProject: ['*.config.*s'] },
 				tsconfigRootDir: import.meta.dirname,
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				ecmaFeatures: {
+					jsx: true,
+				},
 			},
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-unused-vars': 'warn',
 		},
 	}
 );
